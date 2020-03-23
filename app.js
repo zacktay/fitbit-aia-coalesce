d88a86d0cbff7a24d465550f4bc5849d54d5ac51
Zack Tay <tayguangwei@gmail.com>
2020-03-24 00:47:11 +0800

    refactor

--- app.js
+++ app.js
@@ -8,92 +8,84 @@ import aiaData from "./static-data/AIA_Data_23032020";
 const app = express();
 const port = 3000;
 
-const getAiaDataByYear = (year, month) => {
-  const startOfMonth = moment([year, month]).startOf("month").startOf('day');
-  const endOfMonth = moment([year, month]).endOf("month").endOf('day');
-
-  return aiaData
-    .filter(
-      ({ points }) =>
-        (points === "50" || points === "100")
-    )
-    .map(({ awardedDate }) => awardedDate)
-    .map(each => moment(each, "YYYY-MM-DD").endOf("day"))
-    .filter(each => each.isSameOrAfter(startOfMonth) && each.isSameOrBefore(endOfMonth));
-};
-
 const padZero = number => number.toString().padStart(2, "0");
 
+const processRow = row => ({
+  date: moment(row[0], "D/MM/YYYY").endOf("day"),
+  steps: row[2] ? Number(row[2].replace(",", "")) : null
+});
+
+const getAiaDataByYearAndMonth = (year, month) => {
+  const startOfMonth = moment([year, month])
+    .startOf("month")
+    .startOf("day");
+  const endOfMonth = moment([year, month])
+    .endOf("month")
+    .endOf("day");
+
+  return aiaData
+    .filter(({ points }) => points === "50" || points === "100")
+    .map(({ awardedDate }) => awardedDate)
+    .map(each => moment(each, "YYYY-MM-DD").endOf("day"))
+    .filter(
+      each =>
+        each.isSameOrAfter(startOfMonth) && each.isSameOrBefore(endOfMonth)
+    );
+};
+
+const getDiff = (aiaData, result) => {
+  const fitbitData = result.slice(1, result.length - 1);
+  // In Fitbit but not in AIA
+  const diff = fitbitData
+    .filter(
+      ({ date, steps }) =>
+        steps >= 10000 && !aiaData.find(aiaDate => aiaDate.isSame(date, "day"))
+    )
+    .map(({ date, steps }) => ({
+      steps: steps,
+      date: date.toLocaleString()
+    }));
+
+  return {
+    fitbit: fitbitData,
+    aia: aiaData,
+    diff
+  };
+};
+
 // Methods
+// Default - Current Year and Month
 app.get("/", (req, res) => {
   const now = moment();
   const path = `./static-data/${now.year()}_${padZero(now.month() + 1)}.csv`;
   const result = [];
-  // Reads CSV for particular year and month
+
   fs.createReadStream(path)
     .pipe(csv())
-    .on("data", row =>
-      result.push({
-        date: moment(row[0], "D/MM/YYYY").endOf('day'),
-        steps: row[2] ? Number(row[2].replace(",", "")) : null
-      })
-    )
+    .on("data", row => result.push(processRow(row)))
     .on("end", () => {
-      const aiaData = getAiaDataByYear(now.year(), now.month());
-      const fitbitData = result.slice(1, result.length - 1);
-      // In Fitbit but not in AIA
-      const diff = fitbitData
-        .filter(
-          ({ date, steps }) =>
-            steps >= 10000 && !aiaData.find(aiaDate => aiaDate.isSame(date, 'day'))
-        )
-        .map(({ date, steps }) => ({
-          steps: steps,
-          date: date.toLocaleString()
-        }));
+      const aiaData = getAiaDataByYearAndMonth(now.year(), now.month());
+      const response = getDiff(aiaData, result);
 
-      res.status(200).send({
-        fitbit: fitbitData,
-        aia: aiaData,
-        diff
-      });
+      res.status(200).send(response);
     });
 });
 
+// Specific Year and Month
 app.get("/:year/:month", (req, res) => {
   const yearParam = req.params.year;
   const monthParam = req.params.month;
-
   const path = `./static-data/${yearParam}_${monthParam}.csv`;
   const result = [];
-  // Reads CSV for particular year and month
+
   fs.createReadStream(path)
     .pipe(csv())
-    .on("data", row =>
-      result.push({
-        date: moment(row[0], "D/M/YYYY").endOf("day"),
-        steps: row[2] ? Number(row[2].replace(",", "")) : null
-      })
-    )
+    .on("data", row => result.push(processRow(row)))
     .on("end", () => {
-      const aiaData = getAiaDataByYear(yearParam, monthParam - 1);
-      const fitbitData = result.slice(1, result.length - 1);
-      // In Fitbit but not in AIA
-      const diff = fitbitData
-        .filter(
-          ({ date, steps }) =>
-            steps >= 10000 && !aiaData.find(aiaDate => aiaDate.isSame(date, 'day'))
-        )
-        .map(({ date, steps }) => ({
-          steps,
-          date: date.toLocaleString()
-        }));
+      const aiaData = getAiaDataByYearAndMonth(now.year(), now.month());
+      const response = getDiff(aiaData, result);
 
-      res.status(200).send({
-        fitbit: fitbitData,
-        aia: aiaData,
-        diff
-      });
+      res.status(200).send(response);
     });
 });
 
